d39873377655a73853888c51e73109344a471e0c
Aqib Razzaq <aqibrazzaq1997@gmail.com>
2022-02-28 17:08:13 +0500

    Update settings.py

--- pharm/settings.py
+++ pharm/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
 ALLOWED_HOSTS = [
-    'pharmacy-management-system2.herokuapp.com',
+    'sas-pharmacy.herokuapp.com',
     '127.0.0.1'
 ]
 
